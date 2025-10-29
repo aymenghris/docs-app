facc57d1e4fbb3c75dff4ea5e2515c861254a273
aymenghris <aymengharis@gmail.com>
2025-10-29 10:28:34 +0100

    feat(editor): integrate global state management and add FontFamily & TextStyle extensions

--- app/documents/[documentId]/Editor.tsx
+++ app/documents/[documentId]/Editor.tsx
@@ -1,13 +1,31 @@
 'use client'
 
+import { useEditorStore } from '@/store/use-editor'
 import { TaskItem, TaskList } from '@tiptap/extension-list'
 import { TableKit } from '@tiptap/extension-table'
+import { FontFamily, TextStyle } from '@tiptap/extension-text-style'
 import { EditorContent, useEditor } from '@tiptap/react'
 import StarterKit from '@tiptap/starter-kit'
 import ImageResize from 'tiptap-extension-resize-image'
 
 export const Editor = () => {
+    const { setEditor } = useEditorStore()
+
+    const handleEditor = ({ editor }: { editor: any }) => {
+        setEditor(editor)
+    }
+
     const editor = useEditor({
+        onCreate: handleEditor,
+        onUpdate: handleEditor,
+        onSelectionUpdate: handleEditor,
+        onTransaction: handleEditor,
+        onFocus: handleEditor,
+        onBlur: handleEditor,
+        onContentError: handleEditor,
+        onDestroy() {
+            setEditor(null)
+        },
         editorProps: {
             attributes: {
                 class: 'print:border-0 bg-white border border-[#c7c7c7] flex flex-col min-h-[1054px] w-[816px] py-10 pr-14 cursor-text  focus:outline-none',
@@ -15,6 +33,7 @@ export const Editor = () => {
             },
         },
         extensions: [
+            FontFamily,
             ImageResize,
             StarterKit,
             TaskList,
@@ -24,6 +43,7 @@ export const Editor = () => {
             TableKit.configure({
                 table: { resizable: true },
             }),
+            TextStyle,
         ],
         content: '<p>Hello World 🔥</p>',
         immediatelyRender: false,
