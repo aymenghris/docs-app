51707d935a0cf538a139f51248a0210a1d8fcc56
aymenghris <aymengharis@gmail.com>
2025-10-29 10:24:35 +0100

    feat(toolbar): add Tools section with Comment, List Todo, and Remove Formatting buttons

---
+++ app/documents/[documentId]/toolbar/buttons/sections/Tools.tsx
@@ -0,0 +1,34 @@
+import { FC } from 'react'
+import { Button } from '@toolbar/buttons/Button'
+import { ButtonWrapper } from '@toolbar/utils/types'
+import {
+    ListTodoIcon,
+    MessageSquarePlusIcon,
+    RemoveFormattingIcon,
+} from 'lucide-react'
+
+export const AddCommentButton: FC<ButtonWrapper> = ({ editor }) => (
+    <Button
+        label="Comment"
+        icon={MessageSquarePlusIcon}
+        onClick={() => console.log('Comment')}
+        isActive={false} // TODO: Implement this functionality
+    />
+)
+
+export const ListTodoButton: FC<ButtonWrapper> = ({ editor }) => (
+    <Button
+        label="List Todo"
+        icon={ListTodoIcon}
+        onClick={() => editor?.chain().focus().toggleTaskList().run()}
+        isActive={editor?.isActive('taskList')}
+    />
+)
+
+export const RemoveFormattingButton: FC<ButtonWrapper> = ({ editor }) => (
+    <Button
+        label="Remove Formatting"
+        icon={RemoveFormattingIcon}
+        onClick={() => editor?.chain().focus().unsetAllMarks().run()}
+    />
+)
