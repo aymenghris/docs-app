0cf1f3651417d60109402b745ae63313c13cb2f3
aymenghris <aymengharis@gmail.com>
2025-10-29 10:26:36 +0100

    feat(toolbar): create sectionsButtons array to organize toolbar buttons by sections

---
+++ app/documents/[documentId]/toolbar/buttons/index.ts
@@ -0,0 +1,25 @@
+import { FontFamilyButton, HeadingLevelButton } from '@toolbar/buttons/custom'
+import {
+    PrintButton,
+    RedoButton,
+    SpellCheckButton,
+    UndoButton,
+} from '@toolbar/buttons/sections/Actions'
+import {
+    BoldToggleButton,
+    ItalicToggleButton,
+    UnderlineToggleButton,
+} from '@toolbar/buttons/sections/TextFormatting'
+import {
+    AddCommentButton,
+    ListTodoButton,
+    RemoveFormattingButton,
+} from '@toolbar/buttons/sections/Tools'
+
+export const sectionsButtons = [
+    [UndoButton, RedoButton, PrintButton, SpellCheckButton],
+    [FontFamilyButton],
+    [HeadingLevelButton],
+    [BoldToggleButton, ItalicToggleButton, UnderlineToggleButton],
+    [AddCommentButton, ListTodoButton, RemoveFormattingButton],
+]
