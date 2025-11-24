0cd0727f3a7dd43faed4f38dba8fdb16bbe04eab
aymenghris <aymengharis@gmail.com>
2025-11-24 10:00:00 +0100

    refactor(navbar): replace Image component with the newly created Logo component

--- app/documents/[documentId]/navbar/Navbar.tsx
+++ app/documents/[documentId]/navbar/Navbar.tsx
@@ -1,15 +1,12 @@
+import { Logo } from '@/components/Logo'
 import { DocumentInput } from '@navbar/DocumentInput'
 import { MenuBar } from '@navbar/MenuBar'
-import Image from 'next/image'
-import Link from 'next/link'
 
 export const Navbar = () => {
     return (
         <nav className="flex items-center justify-between">
             <div className="flex items-center gap-2">
-                <Link href="/public">
-                    <Image src="/logo.svg" alt="logo" width={36} height={36} />
-                </Link>
+                <Logo />
                 <div className="flex flex-col">
                     <DocumentInput />
                     <MenuBar />
