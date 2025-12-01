3d85848b9d843ad5d5a86bdff970859ff81eae43
aymenghris <aymengharis@gmail.com>
2025-12-01 08:41:38 +0100

    feat(convex): add create document mutation and pagination support for listing documents
    - Introduce `create` mutation for adding new documents with optional title and content.
    - Enhance `list` query with pagination support using `paginationOptsValidator`.

--- convex/documents.ts
+++ convex/documents.ts
@@ -1,8 +1,28 @@
+import { paginationOptsValidator } from 'convex/server'
+import { ConvexError, v } from 'convex/values'
 import { query } from './_generated/server'
+import { mutation } from './_generated/server'
 
-export const listDocuments = query({
-    args: {},
-    handler: async (ctx) => {
-        return await ctx.db.query('documents').collect()
+export const create = mutation({
+    args: {
+        title: v.optional(v.string()),
+        initialContent: v.optional(v.string()),
+    },
+    handler: async (ctx, args) => {
+        const user = await ctx.auth.getUserIdentity()
+        if (!user) throw new ConvexError('Unauthorized')
+
+        return await ctx.db.insert('documents', {
+            title: args.title ?? 'Untitled Document',
+            ownerId: user.subject,
+            initialContent: args.initialContent,
+        })
+    },
+})
+
+export const list = query({
+    args: { paginationOpts: paginationOptsValidator },
+    handler: async (ctx, args) => {
+        return await ctx.db.query('documents').paginate(args.paginationOpts)
     },
 })
