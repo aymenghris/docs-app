3d8d57418cef8bf5af3a9d9ab9b6ca258112a07d
aymenghris <aymengharis@gmail.com>
2025-12-01 08:40:33 +0100

    feat(auth): enhance ConvexClientProvider with Clerk auth and TanStack Query integration
    - Wrap ConvexClientProvider with ClerkProvider for authentication.
    - Add support for authenticated, unauthenticated, and auth loading states.
    - Integrate TanStack Query with Convex for optimized query management.
    - Include FullScreenLoader during authentication processes.

--- components/ConvexClientProvider.tsx
+++ components/ConvexClientProvider.tsx
@@ -1,10 +1,48 @@
 'use client'
 
 import { ReactNode } from 'react'
-import { ConvexProvider, ConvexReactClient } from 'convex/react'
+import { FullScreenLoader } from '@/components/FullScreenLoader'
+import { ClerkProvider, SignIn, useAuth } from '@clerk/nextjs'
+import { ConvexQueryClient } from '@convex-dev/react-query'
+import { QueryClient } from '@tanstack/query-core'
+import { QueryClientProvider } from '@tanstack/react-query'
+import {
+    Authenticated,
+    AuthLoading,
+    ConvexReactClient,
+    Unauthenticated,
+} from 'convex/react'
+import { ConvexProviderWithClerk } from 'convex/react-clerk'
 
 const convex = new ConvexReactClient(process.env.NEXT_PUBLIC_CONVEX_URL!)
 
+const convexQueryClient = new ConvexQueryClient(convex)
+const queryClient = new QueryClient({
+    defaultOptions: {
+        queries: {
+            queryKeyHashFn: convexQueryClient.hashFn(),
+            queryFn: convexQueryClient.queryFn(),
+        },
+    },
+})
+convexQueryClient.connect(queryClient)
+
 export function ConvexClientProvider({ children }: { children: ReactNode }) {
-    return <ConvexProvider client={convex}>{children}</ConvexProvider>
+    return (
+        <ClerkProvider>
+            <ConvexProviderWithClerk client={convex} useAuth={useAuth}>
+                <QueryClientProvider client={queryClient}>
+                    <Authenticated>{children}</Authenticated>
+                    <Unauthenticated>
+                        <div className="flex min-h-screen flex-col items-center justify-center">
+                            <SignIn routing="hash" />
+                        </div>
+                    </Unauthenticated>
+                    <AuthLoading>
+                        <FullScreenLoader label="Authenticating..." />
+                    </AuthLoading>
+                </QueryClientProvider>
+            </ConvexProviderWithClerk>
+        </ClerkProvider>
+    )
 }
