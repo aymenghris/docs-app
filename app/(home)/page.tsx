88d3c55b068d1d0238cb6b50e212344f6d814aab
aymenghris <aymengharis@gmail.com>
2025-12-01 15:52:25 +0100

    feat(home): integrate DocumentsTable into the home page
    - Display documents with pagination using the ` usePaginatedDocuments ` hook.
    - Update imports for reorganized `Navbar` and `TemplatesGallery` folders.

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -1,7 +1,13 @@
-import { Navbar } from '@home/Navbar'
-import { TemplatesGallery } from '@home/TemplatesGallery'
+'use client'
+
+import { usePaginatedDocuments } from '@/hooks/usePaginatedDocuments'
+import { DocumentsTable } from '@home/documents-table/DocumentsTable'
+import { Navbar } from '@home/navbar/Navbar'
+import { TemplatesGallery } from '@home/templates/TemplatesGallery'
 
 const Home = () => {
+    const { results, status, loadMore, isLoading } = usePaginatedDocuments()
+
     return (
         <div className="flex min-h-screen flex-col">
             <div className="fixed inset-x-0 top-0 z-10 h-16 bg-white p-4">
@@ -9,6 +15,12 @@ const Home = () => {
             </div>
             <div className="mt-16">
                 <TemplatesGallery />
+                <DocumentsTable
+                    documents={results}
+                    loadMore={loadMore}
+                    status={status}
+                    isLoading={isLoading}
+                />
             </div>
         </div>
     )
