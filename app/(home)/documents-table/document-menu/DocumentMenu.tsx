559c3c1dd0c162ad0bc84bacadf63d8bd8a11275
aymenghris <aymengharis@gmail.com>
2025-12-04 09:47:57 +0100

    feat(home): add Rename option to DocumentMenu and refactor menu structure
    - Integrate `RenameDocumentDialog` for renaming document titles.
    - Refactor `DocumentMenu` to streamline item handling and improve code organization.
    - Move and update imports to align with the `document-menu` folder structure.

--- app/(home)/documents-table/document-menu/DocumentMenu.tsx
+++ app/(home)/documents-table/document-menu/DocumentMenu.tsx
@@ -6,31 +6,26 @@ import {
     DropdownMenuItem,
     DropdownMenuTrigger,
 } from '@/components/ui/dropdown-menu'
-import { DeleteDocumentDialog } from '@home/documents-table/DeleteDocumentDialog'
-import { DocumentId } from '@home/documents-table/types'
-import { ExternalLinkIcon, MoreVertical, Trash2Icon } from 'lucide-react'
-
-const OpenInNewTabMenuItem: FC<{ documentId: DocumentId }> = ({
-    documentId,
-}) => (
-    <DropdownMenuItem
-        onClick={() =>
-            window.open(
-                `/documents/${documentId}`,
-                '_blank',
-                'noopener,noreferrer',
-            )
-        }
-    >
-        <ExternalLinkIcon className="mr-2" />
-        Open in a new tab
-    </DropdownMenuItem>
-)
+import { DeleteDocumentDialog } from '@home/documents-table/document-menu/DeleteDocumentDialog'
+import { RenameDocumentDialog } from '@home/documents-table/document-menu/RenameDocumentDialog'
+import { DocumentId } from '@home/documents-table/document-menu/types'
+import {
+    ExternalLinkIcon,
+    FilePenIcon,
+    MoreVertical,
+    Trash2Icon,
+} from 'lucide-react'
 
 export const DocumentMenu: FC<{ documentId: DocumentId; title: string }> = ({
     documentId,
+    title,
 }) => {
     const [showDeleteDialog, setShowDeleteDialog] = useState(false)
+    const [showRenameDialog, setShowRenameDialog] = useState(false)
+
+    const handleOpenInNewTab = () => {
+        window.open(`/documents/${documentId}`, '_blank', 'noopener,noreferrer')
+    }
 
     return (
         <>
@@ -45,10 +40,17 @@ export const DocumentMenu: FC<{ documentId: DocumentId; title: string }> = ({
                     </Button>
                 </DropdownMenuTrigger>
                 <DropdownMenuContent>
-                    <OpenInNewTabMenuItem documentId={documentId} />
+                    <DropdownMenuItem onClick={() => setShowRenameDialog(true)}>
+                        <FilePenIcon />
+                        Rename
+                    </DropdownMenuItem>
                     <DropdownMenuItem onClick={() => setShowDeleteDialog(true)}>
                         <Trash2Icon className="mr-2" />
                         Delete
+                    </DropdownMenuItem>
+                    <DropdownMenuItem onClick={handleOpenInNewTab}>
+                        <ExternalLinkIcon className="mr-2" />
+                        Open in a new tab
                     </DropdownMenuItem>
                 </DropdownMenuContent>
             </DropdownMenu>
@@ -58,6 +60,13 @@ export const DocumentMenu: FC<{ documentId: DocumentId; title: string }> = ({
                 open={showDeleteDialog}
                 onOpenChange={setShowDeleteDialog}
             ></DeleteDocumentDialog>
+
+            <RenameDocumentDialog
+                documentId={documentId}
+                initialTitle={title}
+                open={showRenameDialog}
+                onOpenChange={setShowRenameDialog}
+            />
         </>
     )
 }
