fe38956e8433ef54c0cfc9ee5ce6929e357608a3
aymenghris <aymengharis@gmail.com>
2025-12-05 13:03:01 +0100

    feat(ui): add organization switcher to navbar
    - Include `OrganizationSwitcher` component from Clerk.
    - Update navbar layout to accommodate organization switching alongside user actions.

--- app/(home)/navbar/Navbar.tsx
+++ app/(home)/navbar/Navbar.tsx
@@ -1,5 +1,5 @@
 import { Logo } from '@/components/Logo'
-import { UserButton } from '@clerk/nextjs'
+import { OrganizationSwitcher, UserButton } from '@clerk/nextjs'
 import { SearchInput } from '@home/navbar/SearchInput'
 
 export const Navbar = () => {
@@ -10,7 +10,10 @@ export const Navbar = () => {
                 <h3 className="text-xl">Docs</h3>
             </div>
             <SearchInput />
-            <UserButton />
+            <div className="flex items-center gap-3 pl-6">
+                <OrganizationSwitcher />
+                <UserButton />
+            </div>
         </nav>
     )
 }
