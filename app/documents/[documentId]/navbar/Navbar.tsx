e90cd81dd6267fa7fab25ef415e1b052a995d262
aymenghris <aymengharis@gmail.com>
2025-12-11 20:15:41 +0100

    feat(ui): enhance `Navbar` with `SeparatedContainer`, `NotificationsInbox`, and conditional `Avatars`
    - Refactor layout using `SeparatedContainer` for consistent item spacing.
    - Add `NotificationsInbox` for real-time notifications.
    - Display `Avatars` conditionally based on active users with `Liveblocks`.

--- app/documents/[documentId]/navbar/Navbar.tsx
+++ app/documents/[documentId]/navbar/Navbar.tsx
@@ -1,10 +1,18 @@
+'use client'
+
 import { AuthenticationControls } from '@/components/AuthenticationControls'
 import { Logo } from '@/components/Logo'
+import { SeparatedContainer } from '@/components/SeparatedContainer'
+import { useOthers } from '@liveblocks/react'
 import { Avatars } from '@navbar/Avatars'
 import { DocumentInput } from '@navbar/DocumentInput'
 import { MenuBar } from '@navbar/MenuBar'
+import { NotificationsInbox } from '@navbar/notifications/NotificationsInbox'
 
 export const Navbar = () => {
+    const others = useOthers()
+    const hasOthers = others && others.length > 0
+
     return (
         <nav className="flex items-center justify-between">
             <div className="flex items-center gap-2">
@@ -14,9 +22,12 @@ export const Navbar = () => {
                     <MenuBar />
                 </div>
             </div>
-            <div className="flex">
-                <Avatars />
-                <AuthenticationControls />
+            <div className="flex h-10 items-center">
+                <SeparatedContainer className="mx-1">
+                    {hasOthers && <Avatars />}
+                    <NotificationsInbox />
+                    <AuthenticationControls />
+                </SeparatedContainer>
             </div>
         </nav>
     )
