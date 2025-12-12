bf263812dd6a6f3b613df0a4b7eac5c515994474
aymenghris <aymengharis@gmail.com>
2025-12-12 21:45:17 +0100

    feat(stores): add `useDocumentStore` for managing document state with Zustand

---
+++ stores/use-document-store.ts
@@ -0,0 +1,15 @@
+// stores/use-document-store.ts
+import { Doc } from '@/convex/_generated/dataModel'
+import { create } from 'zustand'
+
+type Document = Doc<'documents'>
+
+interface DocumentStore {
+    document: Document | null | undefined
+    setDocument: (document: Document | null | undefined) => void
+}
+
+export const useDocumentStore = create<DocumentStore>((set) => ({
+    document: undefined,
+    setDocument: (document) => set({ document }),
+}))
