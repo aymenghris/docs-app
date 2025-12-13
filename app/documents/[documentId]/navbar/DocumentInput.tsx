2e4a7176587fa6ff7469b43410adafeabfe9bc74
aymenghris <aymengharis@gmail.com>
2025-12-13 09:21:00 +0100

    feat(documents): display the document title in the navbar

--- app/documents/[documentId]/navbar/DocumentInput.tsx
+++ app/documents/[documentId]/navbar/DocumentInput.tsx
@@ -1,10 +1,15 @@
+'use client'
+
+import { useDocumentStore } from '@/stores/use-document-store'
 import { BsCloudCheck } from 'react-icons/bs'
 
 export const DocumentInput = () => {
+    const { document } = useDocumentStore()
+
     return (
         <div className="flex items-center gap-2">
             <span className="cursor-pointer truncate px-1.5 text-lg">
-                Untitled Document
+                {document!.title}
             </span>
             <BsCloudCheck />
         </div>
