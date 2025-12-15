ccc933e46d367a69c83c3bd7b39f8099cdfa1c3f
aymenghris <aymen.quantum@gmail.com>
2025-12-15 11:19:27 +0100

    feat(templates): add `initialContent` property to all templates
    - Introduced `initialContent` field for templates to provide predefined content for documents.
    - Updated all templates to include placeholder content.

--- app/(home)/templates/templates.ts
+++ app/(home)/templates/templates.ts
@@ -3,30 +3,36 @@ export const templates = [
         id: 'blank',
         title: 'blank document',
         imageSrc: '/templates-gallery/blank-document.svg',
+        initialContent: '',
     },
     {
         id: 'software-proposal',
         title: 'software development proposal',
         imageSrc: '/templates-gallery/software-proposal.svg',
+        initialContent: '<h2>Software Development Proposal</h2>',
     },
     {
         id: 'project-proposal',
         title: 'project proposal',
         imageSrc: '/templates-gallery/project-proposal.svg',
+        initialContent: '<h2>Project Proposal</h2>',
     },
     {
         id: 'business-letter',
         title: 'business letter',
         imageSrc: '/templates-gallery/business-letter.svg',
+        initialContent: '<h2>Business Letter</h2>',
     },
     {
         id: 'resume',
         title: 'resume',
         imageSrc: '/templates-gallery/resume.svg',
+        initialContent: '<h2>Resume</h2>',
     },
     {
         id: 'cover-letter',
         title: 'cover letter',
         imageSrc: '/templates-gallery/cover-letter.svg',
+        initialContent: '<h2>Cover Letter</h2>',
     },
 ]
